3ddae10669e3cff1381da6fd195d8eb331353fcd
godwinvc <godwin@swift.local>
2017-05-24 18:20:29 +0530

    Little Slider Pending

--- scripts/home.js
+++ scripts/home.js
@@ -7,13 +7,15 @@ $(window).scroll(function () {
 })
 
 function init() {
-
+    
 }
 
 function navControl() {
     if ($(window).scrollTop() > 20) {
         $('nav').removeClass('navbar-trans');
+        $('.bar1, .bar2, .bar3, .wave1, .wave2, .brandName').addClass('white');
     } else {
         $('nav').addClass('navbar-trans');
+        $('.bar1, .bar2, .bar3, .wave1, .wave2, .brandName').removeClass('white');
     }
 }
